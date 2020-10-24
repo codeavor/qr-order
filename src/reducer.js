3ace612fb57157677bbcc485346669906b933796
Jim Skretas <jimskretas@gmail.com>
2020-10-24 21:13:32 +0300

    Add files for jwt authentication

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,12 @@
 import { connectRouter } from "connected-react-router";
 import { combineReducers } from "redux";
-
+import authReducer from "./reducers/authReducer";
 import menuReducer from "./reducers/menuReducer";
 
 const createRootReducer = (history) =>
   combineReducers({
     router: connectRouter(history),
+    user: authReducer,
     menu: menuReducer,
   });
 export default createRootReducer;
