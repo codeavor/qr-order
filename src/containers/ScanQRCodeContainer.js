c8b2a2486655120f2f7d95bcef2532c0d79e004f
jimskretas <jimskretas@gmail.com>
2021-05-01 16:09:28 +0300

    Update qr-code link

--- src/containers/ScanQRCodeContainer.js
+++ src/containers/ScanQRCodeContainer.js
@@ -24,7 +24,7 @@ export function ScanQRCodeContainer({ errorData }) {
         style={{ minHeight: "100vh" }}
       >
         <img
-          src="https://chart.googleapis.com/chart?cht=qr&chl=http%3A%2F%2F192.168.2.7%3A3000%2Fauthentication%2F1&chs=180x180&choe=UTF-8&chld=L|2"
+          src="https://res.cloudinary.com/jimskretas/image/upload/v1619870641/qr-code.webp"
           rel="nofollow"
           alt="qr code"
         />
